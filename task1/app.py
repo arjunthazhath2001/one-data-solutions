89af58791153d59af15af8d433c3e662da92ab43
Arjun T A <arjunta32@gmail.com>
2025-06-04 22:41:49 +0530

    task2 docker-compose modified

--- task1/app.py
+++ task1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask in CI/CD pipeline!"
+    return "Hello All! from Flask in CI/CD pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
