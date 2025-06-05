2261cca80b3b89ccd70dbcaea6e01c208fc3d3e6
Arjun T A <arjunta32@gmail.com>
2025-06-05 14:28:18 +0530

    testing mail in task1

--- task1/app.py
+++ task1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello All! from Flask in CI/CD pipeline! See u"
+    return "Hello All! from TASK1 in CI/CD pipeline! See u"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
