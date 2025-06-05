414e4cd85083257286a09324bcf41949c4434ba4
Arjun T A <arjunta32@gmail.com>
2025-06-05 17:19:35 +0530

    change

--- task4/app.py
+++ task4/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from TASK 4!"
+    return "Hello ONE DATA from  TASK 4!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
