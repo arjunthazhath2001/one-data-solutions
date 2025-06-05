6c3911d7701e4a71566fbc53066d4eb7ee97c30a
Arjun T A <arjunta32@gmail.com>
2025-06-05 18:24:35 +0530

    final updates

--- task7/app.py
+++ task7/app.py
@@ -7,17 +7,17 @@ app= Flask(__name__)
 @app.route("/")
 def home():
     
-    # simulate processing delay
-    time.sleep(1)
+    # # simulate processing delay
+    # time.sleep(1)
     
-    #random failure
-    if random.random() < 0.5: #random.random generates a num between 0 and 1
-        raise Exception("Random failure")
-    return "Hello! Everything is fine"
+    # #random failure
+    # if random.random() < 0.5: #random.random generates a num between 0 and 1
+    #     raise Exception("Random failure")
+    # return "Hello! Everything is fine"
 
     # PROPOSED FIX(error handling via try-except):
     
-    '''
+    
     try:
         time.sleep(1)
         if random.random()<0.5:
@@ -26,7 +26,6 @@ def home():
     except Exception as e:
         return f"Internal Error: {e}",500
     
-    '''
         
 
 if __name__== "__main__":
